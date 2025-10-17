7c3f13bb792e233f5cc3a1ff3115561e77e0ff38
jorge <jorge@LAPTOP-D8GDLQRL>
2025-10-17 10:07:51 -0400

    se crearon los metodos de las credenciales para darle valor al registrador y para buscar su existencia (parametros arreglados)

--- Services/RegistradorService.cs
+++ Services/RegistradorService.cs
@@ -2,19 +2,19 @@
 using UCNE_Registro_Documentos.DAL;
 using UCNE_Registro_Documentos.Models;
 
-namespace UCNE_Registro_Documentos.Services
+namespace UCNE_Registro_Documentos.Services;
+
+public class RegistradorService(IDbContextFactory<Contexto> DbFactory)
 {
-    public class RegistradorService(IDbContextFactory<Contexto> DbFactory)
+    public async Task <bool> ExisteCredenciales (string nombreUsuario, string contrasena)
     {
-        public async Task <bool> ExisteCredenciales (int registradoId, string registradorNombreUsuario)
-        {
-            await using var contexto = await DbFactory.CreateDbContextAsync();
-            return await contexto.Registradores.AnyAsync(r => r.RegistradorId == registradoId && r.NombreUsuario.Equals(registradorNombreUsuario.Trim()));
-        }
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Registradores.AnyAsync(r => r.NombreUsuario.Equals(nombreUsuario.Trim()) && r.Contrasena.Equals(contrasena));
+    }
 
-        public async Task<Registrador?> BuscarRegistrador (int registradoId, string registradorNombreUsuario)
-        {
-
-        }
+    public async Task<Registrador?> BuscarPorCredenciales (string nombreUsuario, string contrasena)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Registradores.FirstOrDefaultAsync(r => r.NombreUsuario.Equals(nombreUsuario.Trim()) && r.Contrasena.Equals(contrasena));
     }
 }

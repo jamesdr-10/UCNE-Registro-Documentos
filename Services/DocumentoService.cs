f037b6fc39df5ef2b25abcb6f2f3107448202b6d
jorge <jorge@LAPTOP-D8GDLQRL>
2025-10-17 10:45:58 -0400

    Se creo el metodo listar en documentosService

---
+++ Services/DocumentoService.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using UCNE_Registro_Documentos.DAL;
+using UCNE_Registro_Documentos.Models;
+
+namespace UCNE_Registro_Documentos.Services;
+
+public class DocumentoService(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task <List<Documento>> Listar (Expression<Func<Documento, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Documentos
+            .Where(criterio)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+}

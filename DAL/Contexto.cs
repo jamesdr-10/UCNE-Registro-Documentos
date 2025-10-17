32a7bd8257920411c4c67e2da9c748ee3919a09d
jorge <jorge@LAPTOP-D8GDLQRL>
2025-10-17 10:41:33 -0400

    Se agrego la tabla (dbSet) de documentos para usarla en el service

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -8,4 +8,5 @@ public class Contexto : DbContext
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
     public DbSet<Registrador> Registradores { get; set; }
+    public DbSet<Documento> Documentos { get; set; }
 }

dd6ea0fdf55bf31f3d1cb14d16b868792f34edad
jorge <jorge@LAPTOP-D8GDLQRL>
2025-10-17 09:57:36 -0400

    Se renombro el archivo y se creo el metodo para verificar la existencia de las credenciales

---
+++ Services/RegistradorService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UCNE_Registro_Documentos.DAL;
+using UCNE_Registro_Documentos.Models;
+
+namespace UCNE_Registro_Documentos.Services
+{
+    public class RegistradorService(IDbContextFactory<Contexto> DbFactory)
+    {
+        public async Task <bool> ExisteCredenciales (int registradoId, string registradorNombreUsuario)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Registradores.AnyAsync(r => r.RegistradorId == registradoId && r.NombreUsuario.Equals(registradorNombreUsuario.Trim()));
+        }
+
+        public async Task<Registrador?> BuscarRegistrador (int registradoId, string registradorNombreUsuario)
+        {
+
+        }
+    }
+}
